0c7bd159da7c92a2fa59a4392e77f510d53375f5
Tomas Roj <rojtomas@email.cz>
2019-04-24 16:57:34 +0200

    added watchify but having one bug - fix needed

--- commandLine.js
+++
@@ -1,9 +0,0 @@
-var emoji = require("node-emoji");
-var readline = require("readline");
-
-var rl = readline.createInterface(process.stdin, process.stdout);
-
-rl.question("Enter your emoji please: "), function(answer){
-    var emo = "💻";
-    console.log(emoji.find(emo));
-}
--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 var emoji = require("node-emoji");
+var $ = require("jquery");
 
-console.log(emoji.find("💻"));
+function clickEvent(){
+    
+    var val = $.trim($("textarea").val());
+      if (val != "") {
+          var emo = emoji.find(val);
+      }
+  document.getElementById("demo").innerHTML = emo;
+  console.log(emo);
+}
